ea74cc294c47493bf3f4a8fd6690425be44d2cbf
boyko-marina <boyko.marina.a@mail.ru>
2025-09-25 20:26:55 +0300

    only_letters_norm_compare

--- onegin.cpp
+++ onegin.cpp
@@ -16,7 +16,7 @@ int compare_letters(char par1, char par2);
 size_t str_len(const char *s);
 int min(int a, int b);
 char *make_letters(char *par1);
-int compare_strings(char *par1, char *par2);
+int compare_strings(const char *par1, const char *par2);
 int compare_strings_reverse(char *par1, char *par2);
 void sort_strings_reverse(char **ptr_text, int k);
 void print_string(char *str);
@@ -45,16 +45,18 @@ int main( )
     sort_strings(ptr_text, k);
     print_text(ptr_text, k);
     putchar('\n');
-
+/*
     printf(COLOR_CYAN "Sorted text by end.\n" COLOR_RESET);
     sort_strings_reverse(ptr_text, k);
     print_text(ptr_text, k);
     putchar('\n');
+*/
 
 //    printf("end of program\n");
 
     free (ptr_text);
     free (text);
+    fclose(fp);
 }
 
 void print_string(char *str)
@@ -114,7 +116,7 @@ char *make_letters(char *par1)
             k++;
         }
     }
-    char* s1 = (char*) calloc(k+1, sizeof(char));
+    char *s1 = (char *) calloc(k+1, sizeof(char));
     int q = 0;
     for (int j = 0; j < len; j++)
     {
@@ -128,41 +130,42 @@ char *make_letters(char *par1)
     return s1;
 }
 
-int compare_strings(char *par1, char *par2)
+int compare_strings(const char *par1, const char *par2)
 {
-    char *s1 = make_letters(par1);
-    char *s2 = make_letters(par2);
+    while (1) {
+        while (*par1 != '\0' && !isalpha((unsigned char) *par1)) par1++;
+        while (*par2 != '\0' && !isalpha((unsigned char) *par2)) par2++;
 
-    // printf("letters1: ");
-    // print_string(s1);
-    // printf("letters2: ");
-    // print_string(s2);
+        if (*par2 == '\0' && *par1 == '\0')
+        {
+            return 0;
+        }
 
-    int len1 = (int) str_len(s1);
-    int len2 = (int) str_len(s2);
-    int len = min(len1, len2);
+        if (*par1 == '\0')
+        {
+            return 0;
+        }
 
-    // printf("len1: %d\n", len1);
-    // printf("len2: %d\n", len2);
-
-    int i = 0;
-    for (; i < len; i++)
-    {
-        int cmp_result = compare_letters(s1[i], s2[i]);
-        if (cmp_result == 1)
+        if (*par2 == '\0')
         {
             return 1;
         }
-        else if(cmp_result == -1)
-        {
-            return -1;
-        }
-    }
 
-    // printf("returning 0");
-    free(s1);
-    free(s2);
-    return 0;
+        char c1 = toupper((unsigned char) *par1);
+        char c2 = toupper((unsigned char) *par2);
+
+        if (c1 > c2)
+        {
+            return 1;
+        }
+        if (c1 < c2)
+        {
+            return 0;
+        }
+
+        par1++;
+        par2++;
+    }
 }
 
 void sort_strings(char **ptr_text, int k)
@@ -300,6 +303,8 @@ char *get_array(FILE *fp, int n)
     }
 
     size_t size_of_arr = fread(text, sizeof(char), n, fp);
+    printf("%d\n", n);
+    printf("%d\n", (int) size_of_arr);
     if (size_of_arr != (size_t) n)
     {
         printf("Error, the size of array isn't equal to the size of file.\n");
@@ -324,8 +329,17 @@ int num_of_lines(char *text, int n)
 
 int size_of_file(FILE *fp)
 {
+    long pos = ftell(fp);
     fseek(fp, 0L, SEEK_END);
     int n = ftell(fp);
-    fseek(fp, 0L, SEEK_SET);
+    fseek(fp, pos, SEEK_SET);
     return n;
 }
+
+// int size_of_file(FILE *fp)
+// {
+//     fseek(fp, 0L, SEEK_END);
+//     int n = ftell(fp);
+//     fseek(fp, 0L, SEEK_SET);
+//     return n;
+// }
